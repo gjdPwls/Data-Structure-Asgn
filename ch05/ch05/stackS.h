07f79b685aac274f6297c188817be6954f7818a5
gjdPwls <133213172+gjdPwls@users.noreply.github.com>
2026-04-12 03:38:13 +0900

    과제 업로드 5주 차

---
+++ ch05/ch05/stackS.h
@@ -0,0 +1,11 @@
+#pragma once
+#define STACK_SIZE 100
+
+typedef int element;		//스택 원소의 자료형을 int로
+element stack[STACK_SIZE];	//1차원 배열 스택 선언
+int isStackEmpty();
+int isStackFull();
+void push(element item);
+element pop();
+element peek();
+void printStack();
